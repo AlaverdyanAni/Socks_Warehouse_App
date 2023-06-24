62cac18aa16dcf06da78af92602d674c28504cff
AlaverdyanAni <ani-alaverdyan@mail.ru>
2023-06-24 17:23:49 +0400

    First commit

---
+++ src/test/java/pro/sky/socks_warehouse_app/SocksWarehouseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.socks_warehouse_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksWarehouseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
